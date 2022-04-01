dbc177f01368a400808df0c6ff9d2a158b99ad1f
Noah Rozario <noahrozario@Noahs-MacBook-Pro.local>
2022-03-31 19:18:02 -0600

    added map and map centering

--- src/components/HouseMap.js
+++ src/components/HouseMap.js
@@ -1,2 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import GoogleMapReact from 'google-map-react'
 
+
+
+const HouseMap = () => {
+
+    const [mapCenter, setMapCenter] = useState({lat: 0, lng: 0})
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition((position) => {
+            setMapCenter({lat: position.coords.latitude, lng: position.coords.longitude})
+        })
+    })
+
+    return (
+        
+        <div style={{height: '90vh', width: '100%'}}>
+            <GoogleMapReact
+            bootstrapURLKeys={{key: "AIzaSyBmdWIAJ1F4Iv8vaJoqH-qznJkO_2omCog"}}
+            center={mapCenter}
+            defaultZoom={12}
+            >
+
+            </GoogleMapReact>
+        </div>
+    )
+}
+
+export default HouseMap
--- src/components/KijijiApplication.js
+++ src/components/KijijiApplication.js
@@ -1,11 +1,14 @@
 import React from "react";
+import HouseMap from "./HouseMap";
 import SearchBar from "./SearchBar";
 
 const KijijiApplication = () => {
 
+
     return (
         <div>
             <SearchBar />
+            <HouseMap />
         </div>
     )
 
